35008a7521cefcf70433cf92ee6cdab8e33174f0
Malyadri Illuri <malyadri@raayansystems.com>
2020-06-16 21:21:50 +0530

    km-bbb-kernel-install.sh updated

--- km-bbb-kernel-install.sh
+++ km-bbb-kernel-install.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash  -e
+
 
 ##############################################
 ############     READ This  ##################
@@ -28,10 +29,10 @@ BCyan='\033[1;36m'        # Cyan
 BWhite='\033[1;37m'       # White
 
 BRedU='\033[4;31m'         # Underline
-
+clear
 temp=$USER
 echo "User Name:$temp"
-
+ KERNEL_UTS=$(cat "include/generated/utsrelease.h" | awk '{print $3}' | sed 's/\"//g' )
 check_mmc () {
         FDISK=$(LC_ALL=C fdisk -l 2>/dev/null | grep "Disk ${media}:" | awk '{print $2}')
 
@@ -43,7 +44,7 @@ check_mmc () {
                 lsblk | grep -v sr0
                 echo ""
                 unset response
-                echo -n "Are you 100% sure, on selecting [${media}] (y/n)? "
+                echo -ne "${Green}Are you 100% sure, on selecting [${media}] (y/n)? ${NC}"
                 read response
                 if [ "x${response}" != "xy" ] ; then
                         exit
@@ -51,24 +52,26 @@ check_mmc () {
                 echo ""
         else
                 echo ""
-                echo "Are you sure? I Don't see [${media}], here is what I do see..."
+                echo -e "${Red}Are you sure? I Don't see [${media}], here is what I do see...${NC}"
                 echo ""
                 echo "lsblk:"
                 lsblk | grep -v sr0
                 echo ""
-                echo "Permission Denied. Run with sudo"
+                echo -e "${Green}Permission Denied. Run with sudo"
                 exit
         fi
 
 }
 
 unmount_all_drive_partitions () {
-        echo ""
+
+	     echo ""
         echo "Unmounting Partitions"
         echo "-----------------------------"
 
         NUM_MOUNTS=$(mount | grep -v none | grep "${media}" | wc -l)
 
+##      for (i=1;i<=${NUM_MOUNTS};i++)
         for ((i=1;i<=${NUM_MOUNTS};i++))
         do
                 DRIVE=$(mount | grep -v none | grep "${media}" | tail -1 | awk '{print $1}')
@@ -78,9 +81,15 @@ unmount_all_drive_partitions () {
 
 
 if [ -z "$1" ]; then
-	echo "usage: sudo $(basename $0) --mmc /dev/sdX --board "
+	echo -e "${Green}usage: sudo $(basename $0) [--mmc /dev/sdX]  [--board ex:1 ] [--scp]${NC}"
 fi
 
+if [ -d out ] ; then
+        echo -e "${Purple}out folder is found${NC}"
+else
+        echo -e "${Red}out folder is not found.${NC}"
+        echo "pls run ./km-bbb-kernel-build.sh"
+fi
 
 # parse commandline options
 while [ ! -z "$1" ] ; do
@@ -91,37 +100,47 @@ while [ ! -z "$1" ] ; do
 	--mmc)
 		media=$2
 		check_mmc
+		unmount_all_drive_partitions
 		sudo mkdir -p /mnt/rootfs
 	        sudo mount ${media}1 /mnt/rootfs
 
-		echo "${Purple}cp out/${KERNEL_UTS}-zImage /mnt/rootfs/boot/vmlinuz-${KERNEL_UTS}${NC}"
-		cp out/${KERNEL_UTS}-zImage /mnt/rootfs/boot/vmlinuz-${KERNEL_UTS}
-		cp out/config-${KERNEL_UTS} /mnt/rootfs/boot/
+		echo -e "${Purple}cp out/${KERNEL_UTS}.zImage /mnt/rootfs/boot/vmlinuz-${KERNEL_UTS}${NC}"
+		sudo cp ./out/${KERNEL_UTS}.zImage  /mnt/rootfs/boot/vmlinuz-${KERNEL_UTS}
+	        sudo cp out/config-${KERNEL_UTS} /mnt/rootfs/boot/
+
+		sudo mkdir -p  /mnt/rootfs/boot/dtbs/${KERNEL_UTS}
+		echo -e "${Purple}cp arch/arm/boot/dts/am335x-boneblack.dtb /mnt/rootfs/boot/dtbs/${KERNEL_UTS} ${NC}"
+		sudo cp arch/arm/boot/dts/am335x-boneblack.dtb  /mnt/rootfs/boot/dtbs/${KERNEL_UTS}/
 		
-		mkdir -p /mnt/rootfs/boot/dtbs/${KENREL_UTS}
-		echo "${Purple}cp arch/arm/boot/dts/am335x-boneblack.dtb /mnt/roots/dtbs/${KERNEL_UTS} ${NC}"
-		cp arch/arm/boot/dts/am335x-boneblack.dtb /mnt/roots/dtbs/${KERNEL_UTS}
-		
-		echo "${Purple} echo uname_r=${KERNEL_UTS} > /mnt/rootfs/boot/uEnv.txt ${NC}"
+		echo -e "${Purple} echo uname_r=${KERNEL_UTS} > /mnt/rootfs/boot/uEnv.txt ${NC}"
 		echo uname_r=${KERNEL_UTS} > /mnt/rootfs/boot/uEnv.txt
                 
-		echo "${Purple} tar -xvf out/${KERNEL_UTS}-modules.tar.gz  -C /mnt/rootfs/ ${NC}"
-                tar -xvf out/${KERNEL_UTS}-modules.tar.gz  -C /mnt/rootfs/
+		echo -e "${Purple} tar -xvf out/${KERNEL_UTS}-modules.tar.gz  -C /mnt/rootfs/ ${NC}"
+                sudo tar -xvf ./out/${KERNEL_UTS}-modules.tar.gz  -C /mnt/rootfs/
         
 		sync
         	unmount_all_drive_partitions
 		;;
         --board)
-		echo "${Purple}cp out/${KERNEL_UTS}-zImage /media/board$2/vmlinuz-${KERNEL_UTS}${NC}"
-		cp out/${KERNEL_UTS}-zImage /media/board$2/vmlinuz-${KERNEL_UTS}
-		echo "${Purple}cp arch/arm/boot/dts/am335x-boneblack.dtb /meida/board$2/${NC}"
-		cp arch/arm/boot/dts/am335x-boneblack.dtb /media/board$2/
-		echo "${Purple} echo uname_r=${KERNEL_UTS} > /media/board$2/uEnv.txt ${NC}"
-		echo uname_r=${KERNEL_UTS} > /media/board$2/uEnv.txt
-		echo "${Purple} echo board_no=$2 >> /media/board$2/uEnv.txti${NC}"
-		echo board_no=$2 >> /media/board$2/uEnv.txt
-		#echo "${Purple}scp /home/$USER/out/vmlinuz-${KERNEL_UTS}  /home/$USER/out/${KERNEL_UTS}-modules.tar.gz /home/$USER/out/${KERNEL_UTS}-dtbs.tar.gz km@192.168.1.1$2:~/$NC"
-                #scp /home/$USER/out/vmlinuz-${KERNEL_UTS}  /home/$USER/out/${KERNEL_UTS}-modules.tar.gz /home/$USER/out/${KERNEL_UTS}-dtbs.tar.gz km@192.168.1.1$2:~/
+		echo -e "${Purple}cp out/${KERNEL_UTS}.zImage /media/board$2/vmlinuz-${KERNEL_UTS}${NC}"
+		sudo cp out/${KERNEL_UTS}.zImage /media/board$2/vmlinuz-${KERNEL_UTS}
+
+		echo -e "${Purple}cp arch/arm/boot/dts/am335x-boneblack.dtb /meida/board$2/${NC}"
+		sudo cp arch/arm/boot/dts/am335x-boneblack.dtb /media/board$2/
+
+		echo -e "${Purple} echo uname_r=${KERNEL_UTS} > /media/board$2/uEnv.txt ${NC}"
+		sudo echo uname_r=${KERNEL_UTS} > /media/board$2/uEnv.txt
+
+		echo -e "${Purple} echo board_no=$2 >> /media/board$2/uEnv.txti${NC}"
+		sudo echo board_no=$2 >> /media/board$2/uEnv.txt
+		;;
+	--scp)
+		echo "pls enter user-name of board"
+		read user_name
+		echo "pls enter ipaddress of board"
+		read ip_addr
+
+		scp out/${KERNEL_UTS}.zImage  ./out/${KERNEL_UTS}-modules.tar.gz ./out/${KERNEL_UTS}-dtbs.tar.gz $user_name@$ip_addr:~/
                 ;;
         esac
         shift
